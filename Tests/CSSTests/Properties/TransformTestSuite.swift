adf03fc5449db8fc1a2a37f7ef0902f2f1498880
Nándor Bereczki <nandmc@hotmail.com>
2026-02-28 12:02:27 +0100

    Refactor Transform property cases to use units and angles + test (#22)

--- Tests/CSSTests/Properties/TransformTestSuite.swift
+++ Tests/CSSTests/Properties/TransformTestSuite.swift
@@ -39,13 +39,13 @@ struct TransformTests {
     @Test
     func values() {
         let matrix = Transform(.matrix(1, 0, 0, 1, 10, 20))
-        let rotate = Transform(.rotate(45))
-        let skew = Transform(.skew(10, 20))
-        let translate = Transform(.translate(12, 24))
+        let rotate = Transform(.rotate(45.deg))
+        let skew = Transform(.skew(10.deg, 20.deg))
+        let translate = Transform(.translate(12.px, 24.px))
         let scale = Transform(.scale3d(1, 2, 3))
-        let rotate3d = Transform(.rotate3d(1, 0, 0, 45))
-        let skewX = Transform(.skewX(15))
-        let perspective = Transform(.perspective(800))
+        let rotate3d = Transform(.rotate3d(1, 0, 0, 45.deg))
+        let skewX = Transform(.skewX(15.deg))
+        let perspective = Transform(.perspective(800.px))
         let matrix3d = Transform(
             .matrix3d(
                 1,
@@ -73,25 +73,25 @@ struct TransformTests {
             renderer.renderProperty(matrix)
                 == "transform: matrix(1.0,0.0,0.0,1.0,10.0,20.0)"
         )
-        #expect(renderer.renderProperty(rotate) == "transform: rotate(45.0deg)")
+        #expect(renderer.renderProperty(rotate) == "transform: rotate(45deg)")
         #expect(
-            renderer.renderProperty(skew) == "transform: skew(10.0deg,20.0deg)"
+            renderer.renderProperty(skew) == "transform: skew(10deg,20deg)"
         )
         #expect(
             renderer.renderProperty(translate)
-                == "transform: translate(12.0,24.0)"
+                == "transform: translate(12px,24px)"
         )
         #expect(
             renderer.renderProperty(scale) == "transform: scale3d(1.0,2.0,3.0)"
         )
         #expect(
             renderer.renderProperty(rotate3d)
-                == "transform: rotate3d(1.0,0.0,0.0,45.0deg)"
+                == "transform: rotate3d(1.0,0.0,0.0,45deg)"
         )
-        #expect(renderer.renderProperty(skewX) == "transform: skewX(15.0deg)")
+        #expect(renderer.renderProperty(skewX) == "transform: skewX(15deg)")
         #expect(
             renderer.renderProperty(perspective)
-                == "transform: perspective(800.0)"
+                == "transform: perspective(800px)"
         )
         #expect(
             renderer.renderProperty(matrix3d)
